07944fc6118d2f5f6a901f7a19a6d5aa0c4a0a53
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-05-06 14:53:54 +0900

    feat : classList로 jsx에 class 넣기, css 추가하기, state 사용법은 공부중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [recommend, setRecommend] = useState('남자 코트 추천')
+  const [udong, setUdong] = useState('강남 우동 맛집')
+  const [jsStudy, setJsStudy] = useState('자바스크립트 공부')
+
   return (
     <div className="App">
-      <div></div>
+      <div className='black-nav'>
+        <h4>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>{recommend}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{udong}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{jsStudy}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
