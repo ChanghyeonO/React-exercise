f85e7eb0762f61f2bc676366f17f2be385827b7f
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-05-06 16:20:17 +0900

    feat : 컴포넌트 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ function App() {
 
   const [listTitle, setListTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '자바스크립트 공부']);
   const [thumb, setThumb] = useState(0);
+  const [modal, setModal] = useState(false);
+
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4>블로그임</h4>
+        <h4>blog</h4>
       </div>
+
       <div className='list'>
+
         <h4>{listTitle[0]}
           <span onClick={() => {
             setThumb(thumb + 1)
@@ -37,8 +41,28 @@ function App() {
         <h4>{listTitle[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <button onClick={() => {
+        let copySort = [...listTitle];
+        copySort.sort();
+        setListTitle(copySort)
+      }}>가나다순 정렬</button>
+
+      {
+        modal == true ? <Modal /> : null
+      }
+
     </div>
   );
+}
+const Modal = () => {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
