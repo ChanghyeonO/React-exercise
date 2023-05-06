e8d3f69228821114200a6459f973e859d1ba1bea
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-05-06 17:20:07 +0900

    feat : map 반복문을 사용해 불필요하게 반복되는 코드를 줄임

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
 
   const [listTitle, setListTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '자바스크립트 공부']);
-  const [thumb, setThumb] = useState(0);
+  const [thumb, setThumb] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
 
   return (
@@ -13,34 +13,26 @@ function App() {
       <div className='black-nav'>
         <h4>blog</h4>
       </div>
-
-      <div className='list'>
-
-        <h4>{listTitle[0]}
-          <span onClick={() => {
-            setThumb(thumb + 1)
-          }}>👍🏻
-          </span>
-          {thumb}
-        </h4>
-
-        <button onClick={() => {
-          const copy = [...listTitle];
-          copy[0] = '여자 코트 추천';
-          setListTitle(copy)
-        }}>클릭
-        </button>
-
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{listTitle[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{listTitle[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {
+        listTitle.map(function (para, i) {
+          return (
+            <div className='list' key={i}>
+              <h4 onClick={() => {
+                modal == false ? setModal(true) : setModal(false)
+              }}>{listTitle[i]}
+              </h4>
+              <span onClick={() => {
+                let copyThumb = [...thumb];
+                copyThumb[i] = copyThumb[i] + 1;
+                setThumb(copyThumb)
+              }}>
+                👍🏻   {thumb[i]}
+              </span>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
 
       <button onClick={() => {
         let copySort = [...listTitle];
@@ -55,6 +47,8 @@ function App() {
     </div>
   );
 }
+
+
 const Modal = () => {
   return (
     <div className='modal'>
